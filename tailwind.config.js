690213336e2b3f82ddb4b5ebd6eec6a296c8ce4e
James Knoke <james.knoke@gmail.com>
2023-09-12 11:46:04 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend:{
+      screens:{
+        sm:'480px',
+        md:'768px',
+        lg:'976px',
+        xl:'1440px',
+      }, 
+      colors:{
+        strongCyan:'hsl(171,66%,44%)',
+        lightBlue:'hsl(233,100%,69%)',
+        darkGrayishBlue:'hsl(210,10%,33%)',
+        grayishBlue:'hsl(201,11%,66%)',
+      },
+      fontFamily:{
+        sans: [ 'Bai Jamjuree', 'san-serif' ]
+      }
+    },
+  },
+  plugins: [],
+}
+
+        // strongCyan:'hsl(171,66%,44%)',
+        // lightBlue:'hsl(233,100%,69%)',
+        // darkGrayishBlue:'hsl(210,10%,33%)',
+        // grayishBlue:'hsl(201,11%,66%)',
+
+        // sm:'480px',
+        // md:'768px',
+        // lg:'976px',
+        // xl:'1440px',
